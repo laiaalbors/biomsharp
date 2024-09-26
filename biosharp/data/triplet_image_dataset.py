7c32949f3714ab6a1487efaeb7e611a4b880095d
laiaalbors <laia.albors.3@gmail.com>
2024-09-26 19:41:51 +0200

    [FIX] Removes unecessary imports.

--- biosharp/data/triplet_image_dataset.py
+++ biosharp/data/triplet_image_dataset.py
@@ -1,8 +1,7 @@
 from torch.utils import data as data
 from torchvision.transforms.functional import normalize
 
-from basicsr.utils import img2tensor, FileClient # TODO: vaig haver de canviar una línia de la funció img2tensor original, hauria de comprovar si encara peta!
-from basicsr.data.transforms import augment
+from basicsr.utils import img2tensor, FileClient
 from basicsr.utils.registry import DATASET_REGISTRY
 
 from biosharp.data import augment, triplet_random_crop, triple_paired_paths_from_lmdb, triple_paired_paths_from_meta_info_file, triple_paired_paths_from_folder
