0349a5d3ee4a7e0519202d7396cc8d0dbdacfb91
laiaalbors <laia.albors.3@gmail.com>
2024-10-02 12:23:42 +0200

    [NEW/UPD] Adds function tensor2img with range [0,563] instead of [0,255]. Updates min,max management in imfrompath.

--- biosharp/utils/img_util.py
+++ biosharp/utils/img_util.py
@@ -1,6 +1,70 @@
 import torch
-import rasterio
 import numpy as np
+
+import warnings
+import rasterio
+from rasterio.errors import NotGeoreferencedWarning
+
+warnings.filterwarnings("ignore", category=NotGeoreferencedWarning)
+
+
+def tensor2img(tensor, rgb2bgr=False, out_type=np.uint8, min_max=(0, 1)):
+    """Convert torch Tensors into image numpy arrays.
+
+    After clamping to [min, max], values will be normalized to [0, 1].
+
+    Args:
+        tensor (Tensor or list[Tensor]): Accept shapes:
+            1) 4D mini-batch Tensor of shape (B x 3/1 x H x W);
+            2) 3D Tensor of shape (3/1 x H x W);
+            3) 2D Tensor of shape (H x W).
+            Tensor channel should be in RGB order.
+        rgb2bgr (bool): Whether to change rgb to bgr.
+        out_type (numpy type): output types. If ``np.uint8``, transform outputs
+            to uint8 type with range [0, 563]; otherwise, float type with
+            range [0, 1]. Default: ``np.uint8``.
+        min_max (tuple[int]): min and max values for clamp.
+
+    Returns:
+        (Tensor or list): 3D ndarray of shape (H x W x C) OR 2D ndarray of
+        shape (H x W). The channel order is BGR.
+    """
+    if not (torch.is_tensor(tensor) or (isinstance(tensor, list) and all(torch.is_tensor(t) for t in tensor))):
+        raise TypeError(f'tensor or list of tensors expected, got {type(tensor)}')
+
+    if torch.is_tensor(tensor):
+        tensor = [tensor]
+    result = []
+    for _tensor in tensor:
+        _tensor = _tensor.squeeze(0).float().detach().cpu().clamp_(*min_max)
+        _tensor = (_tensor - min_max[0]) / (min_max[1] - min_max[0])
+
+        n_dim = _tensor.dim()
+        if n_dim == 4:
+            img_np = make_grid(_tensor, nrow=int(math.sqrt(_tensor.size(0))), normalize=False).numpy()
+            img_np = img_np.transpose(1, 2, 0)
+            if rgb2bgr:
+                img_np = cv2.cvtColor(img_np, cv2.COLOR_RGB2BGR)
+        elif n_dim == 3:
+            img_np = _tensor.numpy()
+            img_np = img_np.transpose(1, 2, 0)
+            if img_np.shape[2] == 1:  # gray image
+                img_np = np.squeeze(img_np, axis=2)
+            else:
+                if rgb2bgr:
+                    img_np = cv2.cvtColor(img_np, cv2.COLOR_RGB2BGR)
+        elif n_dim == 2:
+            img_np = _tensor.numpy()
+        else:
+            raise TypeError(f'Only support 4D, 3D or 2D tensor. But received with dimension: {n_dim}')
+        if out_type == np.uint8:
+            # Unlike MATLAB, numpy.unit8() WILL NOT round by default.
+            img_np = (img_np * 563.0).round()
+        img_np = img_np.astype(out_type)
+        result.append(img_np)
+    if len(result) == 1:
+        result = result[0]
+    return result
 
 
 def img2tensor(imgs, bgr2rgb=True, float32=True):
@@ -33,33 +97,42 @@ def img2tensor(imgs, bgr2rgb=True, float32=True):
         return _totensor(imgs, bgr2rgb, float32)
 
 
-def imfrompath(content_path, float32=True, biomass=True):
+def imfrompath(content_path, float32=True, guide_data="biomass"):
     """Read an image from bytes.
 
     Args:
         content_path (str): Image path.
         float32 (bool): Whether to change to float32., If True, will also norm
             to [0, 1]. Default: True.
-        biomass (bool): Whether we are reading biomass data or not. Default: True. 
+        guide_data (str): Whether we are reading biomass data, sentinel2 or landsat5. 
+            Default: "biomass". Options: ["biomass", "sentinel2", "landsat5"]
 
     Returns:
         ndarray: Loaded image array.
     """
+    allowed_types = {"biomass", "sentinel2", "landsat5"}
+    assert guide_data in allowed_types, f"Invalid data_type: {guide_data}. Must be one of {allowed_types}."
+    
     # read raster
     with rasterio.open(content_path) as dataset:
         img = dataset.read()
     img = np.moveaxis(img, 0, -1) # CHW to HWC
 
-    # define min and max values for the given bands
-    max_values = 563. if biomass else np.array([10000]*img.shape[2])
-    min_values = 0. if biomass else np.array([1]*img.shape[2])
+    # define min and max values for the different types of data
+    guide_ranges = {
+        "biomass": (0., 563.),
+        "sentinel2": (1., 10000.),
+        "landsat5": (0., 1.)
+    }
+    min_value, max_value = guide_ranges.get(guide_data, (None, None))
 
     # (height, width) to (height, width, 1)
     if len(img.shape) == 2:
         img = np.expand_dims(img, axis=-1)
     
     if float32:
-        img = (img - min_values) / (max_values - min_values) # normalize values
+        img = np.clip(img, a_min=min_value, a_max=max_value)
+        img = (img - min_value) / (max_value - min_value) # normalize values
         img = img.astype(np.float32)
         
     return img
