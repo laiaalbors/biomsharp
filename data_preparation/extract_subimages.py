e2cdccaac2ecb1989b2680ead92a70c61fdc64d1
laiaalbors <laia.albors.3@gmail.com>
2024-10-07 10:58:43 +0200

    [UPD] Adds interpolation of missing values in extract_subimages.py.

--- data_preparation/extract_subimages.py
+++ data_preparation/extract_subimages.py
@@ -53,7 +53,7 @@ def main():
     """
 
     opt = {}
-    opt['n_thread'] = 20
+    opt['n_thread'] = 10
     opt['compression_level'] = 3
 
     # HR images
@@ -100,15 +100,51 @@ def extract_subimages(opt):
         sys.exit(1)
 
     img_list = list(scandir(input_folder, full_path=True))
+    print(f"Number of images to crop into subimages: {len(img_list)}")
 
     pbar = tqdm(total=len(img_list), unit='image', desc='Extract')
-    pool = Pool(opt['n_thread'])
-    for path in img_list:
-        pool.apply_async(worker, args=(path, opt), callback=lambda arg: pbar.update(1))
+    worker_with_opt = partial(worker, opt=opt)
+    with Pool(opt['n_thread']) as pool:
+        for _ in pool.starmap(worker_with_opt, zip(img_list)):
+            pbar.update(1)
     pool.close()
     pool.join()
     pbar.close()
     print('All processes done.')
+
+
+def interpolate_with_padding(cropped_img, nan_mask, index, img_name, padding_size=1):
+    if np.any(nan_mask):
+        # Apply mirror padding to the image and the NaN mask
+        padded_img = np.pad(cropped_img, ((padding_size, padding_size), (padding_size, padding_size), (0, 0)), mode='reflect')
+        padded_nan_mask = np.pad(nan_mask, ((padding_size, padding_size), (padding_size, padding_size), (0, 0)), mode='reflect')
+
+        # Create grid of indices
+        a, b = np.indices((padded_img.shape[0], padded_img.shape[1]))
+
+        # Interpolate only for NaN values
+        for band in range(padded_img.shape[2]):
+            valid_pixels = ~padded_nan_mask[:, :, band]  # Mask for valid pixels
+            padded_img[:, :, band] = griddata(
+                (a[valid_pixels], b[valid_pixels]),   # Coordinates of valid pixels
+                padded_img[:, :, band][valid_pixels],        # Values of valid pixels
+                (a, b),                               # Coordinates of all pixels
+                method='linear',
+                fill_value=0                     # Optionally keep border NaN if interpolation cannot fill them
+            )
+        
+        # Remove the padding after interpolation to return to original size
+        cropped_img_filled = padded_img[padding_size:-padding_size, padding_size:-padding_size, :]
+        
+        has_zero = (cropped_img_filled == 0).any()
+        # has_nan = np.isnan(cropped_img_filled).any()
+        if has_zero:
+            print(f'Warning: NaN values still present in cropped_img {index} of {img_name}, put them to 0.', flush=True)
+    
+        return cropped_img_filled
+    
+    else:
+        return cropped_img
 
 
 def worker(path, opt):
@@ -134,7 +170,10 @@ def worker(path, opt):
     # remove the x2, x3, x4 and x8 in the filename for DIV2K
     img_name = img_name.replace('x2', '').replace('x3', '').replace('x4', '').replace('x8', '')
 
-    img = cv2.imread(path, cv2.IMREAD_UNCHANGED)
+    with rasterio.open(path) as dataset:
+        img = dataset.read()
+        profile = dataset.profile
+    img = np.moveaxis(img, 0, -1) 
 
     h, w = img.shape[0:2]
     h_space = np.arange(0, h - crop_size + 1, step)
@@ -148,11 +187,32 @@ def worker(path, opt):
     for x in h_space:
         for y in w_space:
             index += 1
-            cropped_img = img[x:x + crop_size, y:y + crop_size, ...]
-            cropped_img = np.ascontiguousarray(cropped_img)
-            cv2.imwrite(
-                osp.join(opt['save_folder'], f'{img_name}_s{index:03d}{extension}'), cropped_img,
-                [cv2.IMWRITE_PNG_COMPRESSION, opt['compression_level']])
+
+            if not os.path.isfile(osp.join(opt['save_folder'], f'{img_name}_s{index:04d}{extension}')):
+                cropped_img = img[x:x + crop_size, y:y + crop_size, ...]
+                cropped_img = np.ascontiguousarray(cropped_img)
+                
+                #------ interpolate missing values -------
+                nan_mask = np.isnan(cropped_img)
+                cropped_img = interpolate_with_padding(cropped_img, nan_mask, index, img_name)
+                #-----------------------------------------
+                
+                profile.update(
+                    height=cropped_img.shape[0],
+                    width=cropped_img.shape[1],
+                    count=cropped_img.shape[2] if len(cropped_img.shape) == 3 else 1,
+                    compress='lzw'
+                )
+
+                # Save the cropped image using rasterio
+                output_path = osp.join(opt['save_folder'], f'{img_name}_s{index:04d}{extension}')
+                with rasterio.open(output_path, 'w', **profile) as dst:
+                    if cropped_img.ndim == 2:
+                        dst.write(cropped_img, 1)
+                    else:
+                        for band in range(cropped_img.shape[2]):
+                            dst.write(cropped_img[:, :, band], band + 1)
+    
     process_info = f'Processing {img_name} ...'
     return process_info
 
