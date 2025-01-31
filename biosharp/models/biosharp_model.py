f6651d1f7119579fb1ffb27b5ba9f5e6c7793c01
laiaalbors <laia.albors.3@gmail.com>
2025-01-31 19:57:13 +0100

    [UPD/NEW] Converts instance variables to local variables in method global_validation. Adds global_validation_baseline. Changes torch.no_grad() for torch.inference_mode() in method process in class BIOSHARPModel.

--- biosharp/models/biosharp_model.py
+++ biosharp/models/biosharp_model.py
@@ -55,11 +55,13 @@ class BIOSHARPModel(SRModel):
         # model inference
         if hasattr(self, 'net_g_ema'):
             self.net_g_ema.eval()
-            with torch.no_grad():
+            # with torch.no_grad():
+            with torch.inference_mode():
                 self.output = self.net_g_ema(self.img, self.img_gd)
         else:
             self.net_g.eval()
-            with torch.no_grad():
+            # with torch.no_grad():
+            with torch.inference_mode():
                 self.output = self.net_g(self.img, self.img_gd)
             # self.net_g.train()
 
@@ -275,8 +277,8 @@ class BIOSHARPModel(SRModel):
         # zero self.metric_results
         if with_metrics:
             self.metric_results = {metric: 0 for metric in self.metric_results}
-            self.sample_results = {metric: 0 for metric in self.metric_results}
-            self.results = {}
+            sample_results = {metric: 0 for metric in self.metric_results}
+            results = {}
         
         metric_data = dict()
         if use_pbar:
@@ -306,10 +308,9 @@ class BIOSHARPModel(SRModel):
                 del self.gt
             
             # tentative for out of GPU memory
-            del self.lq
-            del self.gd
-            del self.output
-            torch.cuda.empty_cache()
+            del self.lq, self.gd, self.output
+            if idx % 10 == 0:
+                torch.cuda.empty_cache()
 
             if save_img:
                 if self.opt['is_train']:
@@ -329,10 +330,14 @@ class BIOSHARPModel(SRModel):
                 for name, opt_ in self.opt['val']['metrics'].items():
                     metric_result = calculate_metric(metric_data, opt_)
                     self.metric_results[name] += metric_result
-                    self.sample_results[name] = metric_result
-                self.results[gd_path] = self.sample_results.copy()
-                print(f"    {gd_path}: {self.sample_results}", flush=True)
+                    sample_results[name] = metric_result
+                # results[gd_path] = sample_results.copy()
+                print(f"    {gd_path}: {sample_results}", flush=True)
             
+            del sr_img
+            if 'gt_img' in locals():
+                del gt_img
+
             if use_pbar:
                 pbar.update(1)
                 pbar.set_description(f'Test {img_name}')
@@ -348,4 +353,91 @@ class BIOSHARPModel(SRModel):
 
             self._log_validation_metric_values(current_iter, dataset_name, tb_logger)
         
-        return self.results
+        return results
+
+
+    def global_validation_baseline(self, dataloader, current_iter, tb_logger, save_img):
+        dataset_name = dataloader.dataset.opt['name']
+        with_metrics = self.opt['val'].get('metrics') is not None
+        use_pbar = self.opt['val'].get('pbar', False)
+
+        if with_metrics:
+            if not hasattr(self, 'metric_results'):  # only execute in the first run
+                self.metric_results = {metric: 0 for metric in self.opt['val']['metrics'].keys()}
+            # initialize the best metric results for each dataset_name (supporting multiple validation datasets)
+            self._initialize_best_metric_results(dataset_name)
+        
+        # zero self.metric_results
+        if with_metrics:
+            self.metric_results = {metric: 0 for metric in self.metric_results}
+            sample_results = {metric: 0 for metric in self.metric_results}
+            results = {}
+        
+        metric_data = dict()
+        if use_pbar:
+            pbar = tqdm(total=len(dataloader), unit='image')
+
+        total = len(dataloader)
+        for idx, val_data in enumerate(dataloader):
+            print(f"    {idx + 1}/{total}", flush=True)
+
+            gd_path = val_data['gt_path'][0]
+            img_name = osp.splitext(osp.basename(val_data['lq_path'][0]))[0]
+
+            visuals = OrderedDict()
+            visuals['result'] = val_data['upscaled_lq']
+            visuals['gt'] = val_data['gt']
+            sr_img = tensor2img([visuals['result']], out_type=np.uint16)
+            metric_data['img'] = sr_img
+            if 'gt' in visuals:
+                gt_img = tensor2img([visuals['gt']], out_type=np.uint16)
+                metric_data['img2'] = gt_img
+                del self.gt
+            
+            # tentative for out of GPU memory
+            del self.lq, self.gd, self.output
+            if idx % 10 == 0:
+                torch.cuda.empty_cache()
+
+            if save_img:
+                if self.opt['is_train']:
+                    save_img_path = osp.join(self.opt['path']['visualization'], img_name,
+                                             f'{img_name}_{current_iter}.tif')
+                else:
+                    if self.opt['val']['suffix']:
+                        save_img_path = osp.join(self.opt['path']['visualization'], dataset_name,
+                                                 f'{img_name}_{self.opt["val"]["suffix"]}.tif')
+                    else:
+                        save_img_path = osp.join(self.opt['path']['visualization'], dataset_name,
+                                                 f'{img_name}_{self.opt["name"]}.tif')
+                imwrite_rasterio(sr_img, save_img_path)
+
+            if with_metrics:
+                # calculate metrics
+                for name, opt_ in self.opt['val']['metrics'].items():
+                    metric_result = calculate_metric(metric_data, opt_)
+                    self.metric_results[name] += metric_result
+                    sample_results[name] = metric_result
+                # results[gd_path] = sample_results.copy()
+                print(f"    {gd_path}: {sample_results}", flush=True)
+            
+            del sr_img
+            if 'gt_img' in locals():
+                del gt_img
+
+            if use_pbar:
+                pbar.update(1)
+                pbar.set_description(f'Test {img_name}')
+        
+        if use_pbar:
+            pbar.close()
+
+        if with_metrics:
+            for metric in self.metric_results.keys():
+                self.metric_results[metric] /= (idx + 1)
+                # update the best metric result
+                self._update_best_metric_result(dataset_name, metric, self.metric_results[metric], current_iter)
+
+            self._log_validation_metric_values(current_iter, dataset_name, tb_logger)
+        
+        return results
