9d270c594c635d0ec1826bd5e048d714da152a37
laiaalbors <laia.albors.3@gmail.com>
2025-04-19 14:36:01 +0200

    [NEW] Adds Naive and France datasets.

--- biosharp/data/france_biomass_dataset.py
+++ biosharp/data/france_biomass_dataset.py
@@ -16,55 +16,6 @@ from biosharp.utils import img2tensor
 from basicsr.utils.registry import DATASET_REGISTRY
 
 
-def crop_to_match(file_25m, file_100m, mida=2250):
-    """
-    Crops the agb_100m raster to match the bounds of the agb_25m raster and returns the cropped result.
-
-    Parameters:
-        file_25m (str): Path to the 25m reference raster file.
-        file_100m (str): Path to the 100m raster file to be cropped.
-        mida (int): The fixed size of the cropped window (default: 480).
-
-    Returns:
-        np.ndarray: The cropped data from agb_100m.
-        dict: Updated profile of the cropped raster.
-    """
-    with rasterio.open(file_25m) as src_25m:
-        bounds_25m = src_25m.bounds
-
-        with rasterio.open(file_100m) as src_100m:
-            transform_100m = src_100m.transform
-
-            # Calculate the initial window for cropping
-            window = from_bounds(*bounds_25m, transform_100m)
-
-            # Adjust window size to exactly mida x mida
-            if window.width != mida or window.height != mida:
-                # Calculate new offsets to ensure the window is centered around the original
-                col_off = max(window.col_off - (mida - window.width) // 2, 0)
-                row_off = max(window.row_off - (mida - window.height) // 2, 0)
-
-                # Adjust offsets to avoid going out of bounds
-                if col_off + mida > src_100m.width:
-                    col_off = src_100m.width - mida
-                if row_off + mida > src_100m.height:
-                    row_off = src_100m.height - mida
-
-                window = Window(col_off, row_off, mida, mida)
-
-            profile = src_100m.profile
-            profile.update({
-                'height': mida,
-                'width': mida,
-                'transform': rasterio.windows.transform(window, transform_100m)
-            })
-
-            # Read the cropped area
-            data = src_100m.read(window=window)
-
-    return data
-
-
 def interpolate_patch(patch, nan_mask, padding_size=1):
     # Apply mirror padding to the patch
     padded_patch = np.pad(patch, ((0, 0), (padding_size, padding_size), (padding_size, padding_size)), mode='reflect')
@@ -126,73 +77,71 @@ def process_in_patches(img, nan_mask, patch_size=512, padding_size=1):
 
 
 @DATASET_REGISTRY.register()
-class GlobalBiomassDataset(data.Dataset):
+class FranceBiomassDataset(data.Dataset):
     def __init__(self, opt):
-        super(GlobalBiomassDataset, self).__init__()
+        super(FranceBiomassDataset, self).__init__()
         self.opt = opt
-        csv_path = opt['csv_path']
-        satellite = opt['guide_data']
-        first_row = opt['first_row'] if 'first_row' in opt else 0
-        last_row = opt['last_row'] if 'last_row' in opt else None
-        self.guide_data = opt['guide_data'] # sentinel2 or landsat5
+        self.gt_file, self.lq_file, self.gd_file = opt['dataroot_gt'], opt['dataroot_lq'], opt['dataroot_gd']
+        self.satellite = opt['guide_data']
 
         # Validate the 'satellite' argument
         valid_satellites = {'landsat5', 'sentinel2'}
-        if satellite not in valid_satellites:
-            raise ValueError(f"Invalid satellite: '{satellite}'. Must be one of {valid_satellites}.")
-
-        # data could be a list, DataFrame, or any other structured format
-        self.df = pd.read_csv(csv_path)
-
-        if last_row is None:
-            last_row = len(self.df)
-        
-        self.df = self.df[first_row:last_row].reset_index(drop=True)
-
-        # Drop rows with missing values
-        self.df.dropna(inplace=True)
-
-        self.satellite = satellite
+        if self.satellite not in valid_satellites:
+            raise ValueError(f"Invalid satellite: '{self.satellite}'. Must be one of {valid_satellites}.")
 
 
     def __len__(self):
-        return len(self.df)
+        return 1
 
 
     def __getitem__(self, idx):
-        # Retrieve data point at index `idx`
-        row = self.df.iloc[idx]
-
-        # Open the AGB-25m raster
-        agb_25m = rioxarray.open_rasterio(row["filename_25m"])
+        # Open the AGB-30m raster
+        agb_30m = rioxarray.open_rasterio(self.gt_file)
         
-        # Open the AGB-100m raster and reproject to match the reference
-        agb_100m = crop_to_match(row["filename_25m"], row["filename_100m"])
+        # Open the AGB-100m raster
+        agb_100m = rioxarray.open_rasterio(self.lq_file)
+        agb_100m_w, agb_100m_h = agb_100m.shape[1:]
+        # agb_res_x, agb_res_y = abs(agb_100m.rio.resolution()[0]), abs(agb_100m.rio.resolution()[1])
 
-        # Create the Multispectral-25m raster and reproject to match the reference
-        multispectral_path = row["filename_l5"] if self.satellite=='landsat5' else row["filename_s2"]
-        multispectral_img = rioxarray.open_rasterio(multispectral_path)
-        multispectral_25m = multispectral_img.rio.reproject_match(agb_25m, resampling=Resampling.nearest).values
+        # BioSHARP: Create the Multispectral-25m raster
+        multispectral_25m = rioxarray.open_rasterio(self.gd_file)
+        
+        # Resamplejar multispectral_25m per tenir la 4 vegades més petita que agb_100m
+        multispectral_25m = multispectral_25m.rio.reproject(
+            dst_crs=agb_100m.rio.crs,  # Manté el mateix sistema de coordenades
+            # resolution=(agb_res_x / 4, agb_res_y / 4),
+            shape=(agb_100m_w*4, agb_100m_h*4),
+            resampling=1  # Resampling bilinear (1) o pots provar altres com cúbic (2) o nearest (0)
+        )
+
+        # New properties of multispectral_25m
+        # profile = multispectral_25m.rio.profile
+        transform = multispectral_25m.rio.transform()
+        crs = multispectral_25m.rio.crs
+        width = multispectral_25m.rio.width
+        height = multispectral_25m.rio.height
+        properties_multispectral_25m = {'transform': transform.to_gdal(), 'crs': str(crs), 'width': width, 'height': height}
 
         # Interpolate multispectral data if needed
         if self.satellite=='sentinel2':
+            agb_25m = agb_30m.rio.reproject_match(multispectral_25m, resampling=Resampling.nearest)
             multispectral_nan_mask = np.any(np.isnan(multispectral_25m), axis=0)
             agb_values_at_nans = agb_25m.values[0][multispectral_nan_mask]
             if np.any(agb_values_at_nans > 0):
                 nan_mask = np.isnan(multispectral_25m)
                 multispectral_25m = process_in_patches(multispectral_25m, nan_mask, patch_size=1024, padding_size=1)
-        
+
         # Define min and max values for the different types of data
         guide_ranges = {
             "biomass": (0., 563.),
             "sentinel2": (1., 10000.),
             "landsat5": (0., 1.)
         }
-        gd_min_value, gd_max_value = guide_ranges.get(self.guide_data, (None, None))
+        gd_min_value, gd_max_value = guide_ranges.get(self.satellite, (None, None))
         bio_min_value, bio_max_value = guide_ranges.get('biomass', (None, None))
 
         # Transformations and normalizations
-        gt = np.nan_to_num(agb_25m.values, copy=False, nan=-1.0).transpose(1, 2, 0)
+        gt = np.nan_to_num(agb_30m.values, copy=False, nan=-1.0).transpose(1, 2, 0)
         gt = img2tensor(gt, bgr2rgb=False, float32=True)
         gt = (gt - bio_min_value) / (bio_max_value - bio_min_value)
         
@@ -200,52 +149,39 @@ class GlobalBiomassDataset(data.Dataset):
         lq = img2tensor(lq, bgr2rgb=False, float32=True)
         lq = (lq - bio_min_value) / (bio_max_value - bio_min_value)
         
+        # BioSHARP
         gd = np.nan_to_num(multispectral_25m, copy=False, nan=0).transpose(1, 2, 0)
         gd = img2tensor(gd, bgr2rgb=False, float32=True)
         gd = np.clip(gd, a_min=gd_min_value, a_max=gd_max_value)
         gd = (gd - gd_min_value) / (gd_max_value - gd_min_value)
         
-        return {'idx': idx, 'lq': lq, 'gt': gt, 'gd': gd, 'lq_path': row["filename_100m"], 'gt_path': row["filename_25m"], 'gd_path': multispectral_path}
+        return {'idx': idx, 'lq': lq, 'gt': gt, 'gd': gd, 'lq_path': self.lq_file, 'gt_path': self.gt_file, 'gd_path': self.gd_file, 'properties': properties_multispectral_25m}
 
 
 @DATASET_REGISTRY.register()
-class GlobalBiomassNaiveDataset(data.Dataset):
+class FranceBiomassNaiveDataset(data.Dataset):
     def __init__(self, opt):
-        super(GlobalBiomassNaiveDataset, self).__init__()
+        super(FranceBiomassNaiveDataset, self).__init__()
         self.opt = opt
-        csv_path = opt['csv_path']
-        first_row = opt['first_row'] if 'first_row' in opt else 0
-        last_row = opt['last_row'] if 'last_row' in opt else None
-
-        # data could be a list, DataFrame, or any other structured format
-        self.df = pd.read_csv(csv_path)
-
-        if last_row is None:
-            last_row = len(self.df)
-        
-        self.df = self.df[first_row:last_row].reset_index(drop=True)
-
-        # Drop rows with missing values
-        self.df.dropna(inplace=True)
-
+        self.gt_file, self.lq_file, self.gd_file = opt['dataroot_gt'], opt['dataroot_lq'], opt['dataroot_gd']
+    
 
     def __len__(self):
-        return len(self.df)
+        return 1
 
 
     def __getitem__(self, idx):
-        # Retrieve data point at index `idx`
-        row = self.df.iloc[idx]
-
-        # Open the AGB-25m raster
-        agb_25m = rioxarray.open_rasterio(row["filename_25m"])
+        # Open the AGB-30m raster
+        agb_30m = rioxarray.open_rasterio(self.gt_file)
         
-        # Open the AGB-100m raster and reproject to match the reference
-        agb_100m = crop_to_match(row["filename_25m"], row["filename_100m"])
+        # Open the AGB-100m raster
+        agb_100m = rioxarray.open_rasterio(self.lq_file)
+        agb_100m_w, agb_100m_h = agb_100m.shape[1:]
+        # agb_res_x, agb_res_y = abs(agb_100m.rio.resolution()[0]), abs(agb_100m.rio.resolution()[1])
 
-        # Baseline: Reproject to match the GT
-        # pred_agb_25m = agb_100m.rio.reproject_match(agb_25m, resampling=Resampling.cubic).values
-        
+        # # Baseline: Reproject to match the GT
+        # pred_agb_30m = agb_100m.rio.reproject_match(agb_30m, resampling=Resampling.cubic).values
+
         # Define min and max values for the different types of data
         guide_ranges = {
             "biomass": (0., 563.),
@@ -255,7 +191,7 @@ class GlobalBiomassNaiveDataset(data.Dataset):
         bio_min_value, bio_max_value = guide_ranges.get('biomass', (None, None))
 
         # Transformations and normalizations
-        gt = np.nan_to_num(agb_25m.values, copy=False, nan=-1.0).transpose(1, 2, 0)
+        gt = np.nan_to_num(agb_30m.values, copy=False, nan=-1.0).transpose(1, 2, 0)
         gt = img2tensor(gt, bgr2rgb=False, float32=True)
         gt = (gt - bio_min_value) / (bio_max_value - bio_min_value)
         
@@ -263,11 +199,10 @@ class GlobalBiomassNaiveDataset(data.Dataset):
         lq = img2tensor(lq, bgr2rgb=False, float32=True)
         lq = (lq - bio_min_value) / (bio_max_value - bio_min_value)
 
-        # Baseline
-        # upscaled_lq = np.nan_to_num(pred_agb_25m, copy=False, nan=-1.0).transpose(1, 2, 0)
+        # # Baseline
+        # upscaled_lq = np.nan_to_num(pred_agb_30m, copy=False, nan=-1.0).transpose(1, 2, 0)
         # upscaled_lq = img2tensor(upscaled_lq, bgr2rgb=False, float32=True)
         # upscaled_lq = (upscaled_lq - bio_min_value) / (bio_max_value - bio_min_value)
-        # Per calcular mae entre 100 i 25 tal qual:
         upscaled_lq = np.repeat(np.repeat(lq, 4, axis=1), 4, axis=2)
-        
-        return {'idx': idx, 'lq': lq, 'gt': gt, 'lq_path': row["filename_100m"], 'gt_path': row["filename_25m"], 'upscaled_lq': upscaled_lq}
+
+        return {'idx': idx, 'lq': lq, 'gt': gt, 'lq_path': self.lq_file, 'gt_path': self.gt_file, 'upscaled_lq': upscaled_lq, 'properties': properties_multispectral_25m}
